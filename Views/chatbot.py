26362934c161f52d789ef3597ec45de428e31ab7
Amar <hela3292@hotmail.com>
2024-08-07 20:59:00 +0200

    ddd

--- Views/chatbot.py
+++ Views/chatbot.py
@@ -4,37 +4,65 @@ import streamlit as st
 
 def generate_response(messages):
     bosnian_keywords = [
-        "radno iskustvo", "gdje si radio", "See Contact", "2014", "koje godine si radio u BH Telecom", 
-        "telecom", "Telinvest", "koje godine si radio u Telinvest", "2015", 
-        "koje godine si radio u logosoftu", "koje godine si radio u ataco", 
-        "ataco", "logosoft", "koje godine si radio u foreo", 
-        "koje godine si radio u logosoftu", "logosoft", "payten", 
-        "koje godine si radio u capital market solutions", "koje godine si radio u payten", 
-        "CMS", "foreo", "poslovi", "koji su tvoji budući planovi", 
-        "planovi", "sada", "kako da te kontaktiram", "kontakt", 
+        "radno iskustvo", "gdje si radio", "See Contact", "2014", "koje godine si radio u BH Telecom",
+        "telecom", "Telinvest", "koje godine si radio u Telinvest", "2015",
+        "koje godine si radio u logosoftu", "koje godine si radio u ataco",
+        "ataco", "logosoft", "koje godine si radio u foreo",
+        "koje godine si radio u logosoftu", "logosoft", "payten",
+        "koje godine si radio u capital market solutions", "koje godine si radio u payten",
+        "CMS", "foreo", "poslovi", "koji su tvoji budući planovi",
+        "planovi", "sada", "kako da te kontaktiram", "kontakt",
         "kako se zoves", "ime"
     ]
     german_keywords = [
-        "arbeitsplätze", "wo hast du gearbeitet", "See Contact", "2014", 
-        "in welchem jahr haben sie bei BH Telecom gearbeitet", "telecom", 
-        "Telinvest", "in welchem jahr haben sie bei telinvest gearbeitet", 
-        "in welchem jahr haben sie bei payten gearbeitet", "in welchem jahr haben sie bei ataco gearbeitet", 
-        "ataco", "in welchem jahr haben sie bei logosoft gearbeitet", "logosoft", 
-        "in welchem jahr haben sie bei foreo gearbeitet", "foreo", 
-        "in welchem jahr haben sie bei capital markt solutions gearbeitet", "CMS", 
-        "was sind deine zukunftspläne", "pläne", "sada", "wie kann ich dich kontaktieren", "kontakt", 
+        "arbeitsplätze", "wo hast du gearbeitet", "See Contact", "2014",
+        "in welchem jahr haben sie bei BH Telecom gearbeitet", "telecom",
+        "Telinvest", "in welchem jahr haben sie bei telinvest gearbeitet",
+        "in welchem jahr haben sie bei payten gearbeitet", "in welchem jahr haben sie bei ataco gearbeitet",
+        "ataco", "in welchem jahr haben sie bei logosoft gearbeitet", "logosoft",
+        "in welchem jahr haben sie bei foreo gearbeitet", "foreo",
+        "in welchem jahr haben sie bei capital markt solutions gearbeitet", "CMS",
+        "was sind deine zukunftspläne", "pläne", "sada", "wie kann ich dich kontaktieren", "kontakt",
         "wie heißt du", "name"
     ]
     english_keywords = [
-        "where have you worked", "See Contact", "2014", 
-        "what year did you work at BH Telecom", "telecom", "Telinvest", 
-        "what year did you work at telinvest", "what year did you work at Ataco", 
-        "ataco", "what year did you work at payten", "payten", 
-        "what year did you work at foreo", "what year did you work at Logosoft", 
-        "logosoft", "foreo", "capital market solutions", "CMS", "right now", 
-        "what are your future plans", "plans", "how can i contact you", 
+        "where have you worked", "See Contact", "2014",
+        "what year did you work at BH Telecom", "telecom", "Telinvest",
+        "what year did you work at telinvest", "what year did you work at Ataco",
+        "ataco", "what year did you work at payten", "payten",
+        "what year did you work at foreo", "what year did you work at Logosoft",
+        "logosoft", "foreo", "capital market solutions", "CMS", "right now",
+        "what are your future plans", "plans", "how can i contact you",
         "contact", "what is your name", "name"
     ]
+
+    for msg in messages:
+        content = msg["content"].lower()
+
+        # Prepoznaj ime
+        if any(keyword in content for keyword in ["kako se zoves", "ime"]) and any(keyword in content for keyword in bosnian_keywords):
+            return "Moje ime je Amar Helac"
+        if any(keyword in content for keyword in ["wie heißt du", "name"]) and any(keyword in content for keyword in german_keywords):
+            return "Mein Name ist Amar Helac"
+        if any(keyword in content for keyword in ["what is your name", "name"]) and any(keyword in content for keyword in english_keywords):
+            return "My name is Amar Helac"
+
+        # Prepoznaj buduće planove
+        if any(keyword in content for keyword in ["koji su tvoji budući planovi", "planovi", "cilje"]) and any(keyword in content for keyword in bosnian_keywords):
+            return "Imam veliki interes da se bavim konkretnije Devops dijelom te svi moji planovi vode ka tom cilju"
+        if any(keyword in content for keyword in ["was sind deine zukunftspläne", "pläne", "Ziel"]) and any(keyword in content for keyword in german_keywords):
+            return "Ich habe ein großes Interesse daran, mich konkreter mit dem Devops-Teil auseinanderzusetzen, und alle meine Pläne führen auf dieses Ziel hin"
+        if any(keyword in content for keyword in ["what are your future plans", "plans", "goal"]) and any(keyword in content for keyword in english_keywords):
+            return "I have a great interest in dealing more specifically with the Devops part, and all my plans lead to that goal"
+
+        # Prepoznaj radno mesto
+        if any(keyword in content for keyword in ["koje godine si radio u BH Telecom", "telecom"]) and any(keyword in content for keyword in bosnian_keywords):
+            return "U kompaniji BH Telecom sam radio u periodu od 08/2014 do 12/2014 gdje sam bio na poziciji Technical Support za Residential korisnike, o navednoj poziciji možete pronaći na stranici about me"
+        if any(keyword in content for keyword in ["in welchem jahr haben sie bei BH Telecom gearbeitet", "telecom"]) and any(keyword in content for keyword in german_keywords):
+            return "Ich habe im Zeitraum von 01/2015 bis 07/2015 bei der Firma BH Telecom gearbeitet, wo ich die Position des technischen Supports für Privatanwender innehatte. Informationen zu dieser Position finden Sie auf der Seite Über mich."
+        if any(keyword in content for keyword in ["what year did you work at BH Telecom", "telecom"]) and any(keyword in content for keyword in english_keywords):
+            return "I worked in the BH Telecom company in the period from 01/2015 to 07/2015 where I was in the position of Technical Support for Residential users, you can find about this position on the about me page."
+
 
     # Prolazi kroz sve poruke
     for msg in messages:
