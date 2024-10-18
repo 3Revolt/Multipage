becafd5c7c73fb23acf141f63a6d2d514a669104
Amar <hela3292@hotmail.com>
2024-10-18 23:41:48 +0200

    finalni profil pic

--- Views/about_me.py
+++ Views/about_me.py
@@ -202,7 +202,7 @@ st.markdown(f'## {texts["select_page"]}')
 # --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 with col1:
-    st.image("assets/your_animation.gif", use_column_width=True)
+    st.image("assets/cv.gif", use_column_width=False, width=200)
 
 with col2:
     st.title(texts['title'], anchor=False)
