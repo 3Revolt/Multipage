a9cd16977262a1707898898113bd3b8d5d7ed3a5
Amar <hela3292@hotmail.com>
2024-10-18 23:33:07 +0200

    d

--- Views/about_me.py
+++ Views/about_me.py
@@ -202,7 +202,7 @@ st.markdown(f'## {texts["select_page"]}')
 # --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 with col1:
-    st.markdown('<img src="assets/cv.gif" width="230" style="display: block; margin: auto;">', unsafe_allow_html=True)
+    st.image("assets/cv.gif", width=230)
 
 with col2:
     st.title(texts['title'], anchor=False)
